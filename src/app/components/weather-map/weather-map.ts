831f9d1f8985c31411596b969411f486d1bd1c66
Dushn11 <m.a.bilous2010@gmail.com>
2026-04-09 10:41:44 +0200

    Initial commit

---
+++ src/app/components/weather-map/weather-map.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-map',
+  imports: [],
+  templateUrl: './weather-map.html',
+  styleUrl: './weather-map.scss',
+})
+export class WeatherMap {}
