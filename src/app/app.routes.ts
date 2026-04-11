8ddf81a071db205c961d3e665ffa5f0b1e534021
Dushn11 <m.a.bilous2010@gmail.com>
2026-04-11 23:58:00 +0200

    Настройка рутинга
    dashboard -> settings
    dashboard -> map

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Map } from './pages/map/map';
+import { Settings } from './pages/settings/settings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'map', component: Map },
+  { path: 'settings', component: Settings },
+];
