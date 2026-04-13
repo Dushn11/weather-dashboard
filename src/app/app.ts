7014cf4dc9d5fadfc5ea55d2e7028731edb520c0
Dushn11 <m.a.bilous2010@gmail.com>
2026-04-13 15:31:47 +0200

    test1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './layout/navbar/navbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('weather-dashboard');
 }
+
--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
-import { Dashboard } from '../../pages/dashboard/dashboard';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, Dashboard],
+  imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {}
+  
